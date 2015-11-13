5f12240657568514ded527cc29686dc933a32c29
baak009 <mirna.baak@wur.nl>
2015-11-13 16:20:41 +0100

    cufflinks changes

--- cufflinks.py
+++ cufflinks.py
@@ -13,7 +13,7 @@ import subprocess
 def cufflinks(pros, bam_nm, gtf_nm):
 	""" cufflinks 
 	"""
-	output_nm = 'outcl_%s'%(bam_nm[:-4])
+	output_nm = 'outcl_%s'%(bam_nm[:-11])
 	print "Converting cufflinks step 1"
 	cmd1 = 'cufflinks -p %s -g %s -o %s %s'%(pros, gtf_nm, output_nm, bam_nm)
 	
@@ -21,7 +21,7 @@ def cufflinks(pros, bam_nm, gtf_nm):
 			res1 = subprocess.check_call(cmd1, shell=True)
 			print res1
 			
-			print 'directory created: outcl_%s'%(bam_nm[:-4])
+			print 'directory created: outcl_%s'%(bam_nm[:-11])
 	else:
 		print 'file does not exist'
 	print 'done'
@@ -31,7 +31,7 @@ def cuffmerge(gtf_nm, ref_genome, assemblie):
 	"""
 	print "cuffmerge"
 	cmd1 = 'cuffmerge -g %s -s %s %s'%(gtf_nm, ref_genome, assemblie)
-
+	print assemblie
 	if os.path.exists(assemblie) and os.path.exists('merged_asm/merged.gtf') == False:
 			res1 = subprocess.check_call(cmd1, shell=True)
 			print res1
@@ -45,9 +45,9 @@ def cuffdiff(diff_name, ref_genome, sample_names, bam_files):
 	"""
 	output_nm = 'outdiff_%s'%(diff_name)
 	print "cuffdiff"
-
-	cmd1 = 'cuffdiff -o %s -b %s -L %s -u merged_asm/merged.gtf %s'(output_nm, ref_genome, sample_names, bam_files)
-    print cmd1
+	cmd1 = 'cuffdiff -o %s -b %s -L %s -u merged_asm/merged.gtf %s'%(
+		output_nm, ref_genome, sample_names, bam_files)
+	print cmd1
 	if os.path.exists('merged_asm/merged.gtf') and os.path.exists(output_nm) == False:
 			res1 = subprocess.check_call(cmd1, shell=True)
 			print res1
@@ -63,15 +63,19 @@ if __name__ == "__main__":
 	dirs = os.listdir(path)
 	counter = 1
 	pros = 2
-	file_name = 'M16Am_hisat2.sorted.bam'
+	#file_name = 'M16Am_hisat2.sorted.bam'
+	file_name = 'M12Am_hisat2.sorted.bam'
+	print file_name
 	gtf_name = '/mnt/scratch/baak009/data/ITAG2.4_gene_models.gtf'
 	ref_genome  = '/mnt/scratch/baak009/data/S_lycopersicum_chromosomes.2.50.fa'
 	assemblie = 'assemblies.txt'
-	cufflinks(pros, file_name, gtf_name)
-
+	#cufflinks(pros, file_name, gtf_name)
+	#cuffmerge(gtf_name, ref_genome, assemblie)
 	diff_name = 'testt'
-	sample_names = 'M12Am, M16Am'
+	sample_names = 'M12Am,M16Am'
 	bam_files = './M12Am_hisat2.sorted.bam, ./M16Am_hisat2.sorted.bam'
+
+	cuffdiff(diff_name, ref_genome, sample_names, bam_files)
 	'''
 	for file_name in dirs:
 		if file_name[-11:] == ".sorted.bam" and counter == 1:
