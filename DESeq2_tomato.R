e3be98db5e9d3a888e893ed6c9d8b59e13f4185f
baak009 <mirna.baak@wur.nl>
2016-01-07 09:16:09 +0100

    R scripts edgeR clustering and deseq2 tomato

---
+++ DESeq2_tomato.R
@@ -0,0 +1,79 @@
+# DESEQ2 tomato script
+# DESEQ2
+source("https://bioconductor.org/biocLite.R")
+biocLite("DESeq2")
+library("DESeq2")
+
+directory <- "E:/thesis/outhtseq_tomato2_I_M_samples/" # directory with files # define this
+new_directory <- paste(directory, "output_ImMm/", sep = "")
+dir.create(new_directory, showWarnings = TRUE, recursive = FALSE)
+
+
+logfoldValue <- 1 # define this
+padjValue <- 0.05 # define this
+
+sampleFiles <- grep(".txt", list.files(directory), value = TRUE)
+
+#sampleCondition <-sub("(...*).*","\\1", sampleFiles) 
+#sampleCondition = c("I12","I12", "I16", "I16", "I24", "I24", "M", "M", "M") # define this
+sampleCondition = c("I","I", "I", "I", "I", "I", "M", "M", "M") # define this
+sampleTable <- data.frame(sampleName = sampleFiles,
+                          fileName = sampleFiles,
+                          condition = sampleCondition)
+
+
+ddsHTSeq <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable,
+                                       directory = directory,
+                                       design= ~ condition)
+dds <-DESeq(ddsHTSeq)
+
+dds <- dds[rowSums(counts(dds))>1,] # prefiltering
+#plotDispEsts(dds)
+# specify the reference level
+dds$condition <- relevel(dds$condition, ref = "I12")
+
+#results
+cond <- unique(sampleCondition)
+combination <- combn(cond, 2)
+
+
+
+output = paste(new_directory,'stats', padjValue, logfoldValue,'.txt', sep = '')
+write('HTSEQ-DESeq\nSampleconditions:\n', output)
+
+for (i in 1:ncol(combination)){
+  s = combination[,i]
+  write(paste('combination:',s ,sep = '\t'),output, append = T)
+  
+  res1 <- results(dds, contrast = c("condition", s[1], s[2]))
+  
+  resOrdered <- res1[order(res1$padj),]
+  
+  p <- nrow(subset(resOrdered, padj<padjValue))
+  t <- nrow(subset(resOrdered, padj<padjValue & log2FoldChange < -logfoldValue))
+  v <- nrow(subset(resOrdered, padj<padjValue & log2FoldChange > logfoldValue))
+  
+  write(paste('padj<',padjValue,p ,sep = '\t'),output, append = T)
+  write(paste('LFC >',logfoldValue, t, sep = '\t'), output, append =T)
+  write(paste('LFC <',-logfoldValue, v, '\n', sep = '\t'),output, append =T)
+  resSig <- subset(resOrdered, padj < padjValue & (log2FoldChange < -(logfoldValue) | log2FoldChange > logfoldValue))
+  write.csv(x = resSig, file = paste(new_directory, "sig",s[1],s[2], padjValue, logfoldValue,".csv", sep = ""))
+}
+
+#res
+
+rld <- rlog(dds)
+vsd <- varianceStabilizingTransformation(dds)
+head(assay(rld), 3)
+library("RColorBrewer")
+sampleDists <- dist(t(assay(rld)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- colnames(rld)
+colnames(sampleDistMatrix) <- colnames(rld)
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+library("pheatmap")
+
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
---
+++ EdgeR_clustering.R
@@ -0,0 +1,67 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite("edgeR")
+biocLite("ggplot2")
+
+library('edgeR')
+library('ggplot2')
+dir = "E:/thesis/outhtseq_botrytis_merged/"
+data <- read.delim("E:/thesis/outhtseq_botrytis_merged/htseq_botrytis_merged.txt", row.names = 1, header = F)
+tail(data)
+data <- data[c(0:(nrow(data)-5)),] #remove htseq total counts
+tail(data)
+colnames(data) = c("B16A", "B16B", "I12A", "I12B", "I16A", "I16B", "I24B", "I24D",
+                   "M12", "M16", "M24")
+data <-  data[,0:(ncol(data)-3)] # remove tomato samples
+#data <- data[1:(nrow(data)-30000),]
+
+group <- c('B16', 'B16', 'I12','I12','I16','I16','I24','I24')
+dge <- DGEList(counts = data, group = group)
+#y$samples
+
+
+dge <- estimateCommonDisp(dge)
+dge <- estimateTagwiseDisp(dge)
+dge.cpm <- cpm(dge,log=T,normalized.lib.sizes=T)
+
+genes.cor <- cor(t(dge.cpm),method="pearson")
+
+genes.dist<- as.dist(0.5 - 0.5 * (genes.cor) )
+hc_tree <- hclust (genes.dist, method="complete" )
+
+# pdf(file=paste("patterns_",temperature,".pdf",sep=""),12,8)
+jpeg('E:/thesis/outhtseq_botrytis_merged/hctree.jpg')
+plot(hc_tree, xlab = NULL)
+dev.off()
+
+hc<- cutree(hc_tree, h=0.4)
+table(hc)
+hc <- cutree(hc_tree, h= 0.8)
+
+
+par(mfrow=c(1,1),mgp=c(3,0.5,0),mar=c(5,3,0.75,0.5),tck=-0.025,omi=c(0.1,0.5,0.5,0.1))
+par(las=2)
+#eerste honderd grootste clusters
+for (loop in as.integer(names(sort(table(hc), decr = T)[1:20]))) { 
+  IDs <- names(hc)[hc == loop]
+  df <- data.frame(dge.cpm[rownames(dge.cpm) %in% IDs,])
+  df.scaled<-apply(df,1,scale)
+  names(df.scaled)<-names(df)
+  colnames(df.scaled)<-rownames(df)
+  rownames(df.scaled)<-colnames(df)
+  df.new <- as.data.frame(as.table(as.matrix(df.scaled)))
+  names(df.new) <- c("timepoint","gene","Z_score")
+  #jpeg(paste("E:/thesis/outhtseq_botrytis_merged/clust", loop, ".png", sep=""))
+  print(ggplot(df.new,aes(x=timepoint,y=Z_score)) +
+          geom_point()  + theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+          stat_smooth(se=FALSE,method="loess") +
+          labs(title=paste("cluster",loop,", size:",length(IDs))) +
+          theme(legend.text = element_text(size = 6)))
+  ggsave(paste(dir, "clust", loop, ".png", sep=""), width=5, height = 5, dpi=100)
+  clust1 = row.names(data)[hc==loop]
+  write.csv(dge.cpm[clust1,], file = paste(dir, "clust", loop, ".csv", sep=""))
+}
+clust <- 8
+clust1 = row.names(data)[hc==clust] # get the names of the genes in the cluster
+clust1[1]
+dge.cpm[clust1,] # get the normalized values of the genes. in the cluster. 
+write.csv(dge.cpm[clust1,], file = paste("E:/thesis/outhtseq_botrytis_merged/clust", clust, ".csv", sep=""))
